bd57db356d314c85c713ba0b753139e240a146b6
MBtsky <9250499222@mail.ru>
2019-09-25 20:00:59 +0300

    Auth templates extended

--- src/NotAuthenticated/Authentification/Templates/Input/index.js
+++ src/NotAuthenticated/Authentification/Templates/Input/index.js
@@ -3,6 +3,7 @@ import Password from './password'
 import Name from './name'
 import FailWrapper from './withFails'
 import onInputChange from './onInputChange'
+import FailsHolder from './FailsHolder'
 export const EmailInput = Email
 export const PasswordInput = Password
 export const NameInput = Name
@@ -11,5 +12,4 @@ export const withFails = FailWrapper
 export const FallibleEmailInput = FailWrapper(Email)
 export const FalliblePasswordInput = FailWrapper(Password)
 export const FallibleNameInput = FailWrapper(Name)
-
-export {onInputChange}
+export {onInputChange, FailsHolder}
